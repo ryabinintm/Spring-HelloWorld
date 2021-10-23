e6264db15996c9370594d2a000101c8d9c62e22e
alexander <ryabinintm@yandex.ru>
2021-10-23 14:04:03 +0300

    main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        // В классе App, по аналогии, вызовите еще раз бин HelloWorld,
+        // затем 2 раза вызовите бин cat. Сравните 2 пары бинов по ссылке
+        // и выведите результат в консоль. Для пары HelloWorld должно вернуться true,
+        // для cat - false. Так вы можете увидеть результат того, как работает наш контейнер.
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = applicationContext.getBean(Cat.class);
+        Cat catBean2 = applicationContext.getBean(Cat.class);
+        System.out.printf("Для пары HelloWorld должно вернуться true: %b%n", bean == bean2);
+        System.out.printf("Для пары Cat должно вернуться false: %b%n", catBean1 == catBean2);
     }
 }
